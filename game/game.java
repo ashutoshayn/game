70554c364ca27612ef3b5398f10b645f8a72094d
Ashu <138532112+ashutoshayn@users.noreply.github.com>
2025-07-28 09:53:42 +1200

    Add files via upload
    feat: Created basic game class with Card inner class and deck initialization
    
    - Defined `Card` inner class with `value` and `type` fields
    - Implemented `buildDeck()` method to generate a full 52-card deck
    - Created placeholder method `shuffleDeck()` for future deck shuffling logic
    - Set up initial `startGame()` method to trigger deck building

--- game/game.java
+++ game/game.java
@@ -2,32 +2,53 @@
 /**
  * Write a description of class game here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Ashu
+ * @version 22/07/2025
  */
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
 public class game
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class game
-     */
-    public game()
-    {
-        // initialise instance variables
-        x = 0;
+    private class Card{
+        String value;
+        String type;
+        
+        Card(String value, String type){
+            this.value = value; // creating value of card
+            this.type = type; // creating type of card
+        }
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    //array list to store cards
+        ArrayList<Card> deck;
+        
+    game(){
+        startGame();
+    }
+    
+    public void startGame(){
+        //deck
+        buildDeck();
+    }
+    
+    public void shuffleDeck(){
+        
+    }
+    
+    public void buildDeck(){
+        deck = new ArrayList<Card>();
+        String[] values = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}; 
+        String[] types = {"C", "D", "H", "S"};
+        
+        for (String type : types){
+            for (String value : values){
+                deck.add(new Card(value, type));
+            }
+        }
+        
+        shuffleDeck();
     }
 }
